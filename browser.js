68688eb2ffe1af63f3826cf3486837654918e986
James Halliday <mail@substack.net>
2012-10-05 07:12:26 -0700

    mux-demux in the browser, control system stubbed out

--- browser.js
+++ browser.js
@@ -1,20 +1,33 @@
 var shoe = require('shoe');
 var emitStream = require('emit-stream');
 var JSONStream = require('JSONStream');
-
-var stream = shoe('/sock').pipe(JSONStream.parse([true]));
-var emitter = emitStream(stream);
+var MuxDemux = require('mux-demux');
+var dnode = require('dnode');
 
 var img = document.querySelector('#viewer');
 var crosshairs = document.querySelector('#crosshairs');
+var control = require('./browser/control');
 
-emitter.on('image', function (data) {
-    img.setAttribute('src', 'data:image/png;base64,' + data);
+var mdm = MuxDemux();
+mdm.on('connection', function (c) {
+    if (c.meta === 'emit') {
+        var emitter = emitStream(c.pipe(JSONStream.parse([true])));
+        
+        emitter.on('image', function (data) {
+            img.setAttribute('src', 'data:image/png;base64,' + data);
+        });
+        
+        emitter.on('red', function () {
+            crosshairs.className = 'active';
+            setTimeout(function () {
+                crosshairs.className = '';
+            }, 500);
+        });
+    }
+    else if (c.meta === 'dnode') {
+        var d = dnode();
+        d.on('remote', control);
+        c.pipe(d).pipe(c);
+    }
 });
-
-emitter.on('red', function () {
-    crosshairs.className = 'active';
-    setTimeout(function () {
-        crosshairs.className = '';
-    }, 500);
-});
+mdm.pipe(shoe('/sock')).pipe(mdm);
